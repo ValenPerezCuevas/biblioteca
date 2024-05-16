c004aefe654ef2e0f82487a74d4a73a64dbd52fe
AranchaC <120171267+AranchaC@users.noreply.github.com>
2024-05-16 11:03:45 +0200

    commit prueba paginacion usuarios

--- src/main/java/biblioteca/biblioteca/controladores/controladorUsuario.java
+++ src/main/java/biblioteca/biblioteca/controladores/controladorUsuario.java
@@ -1,11 +1,15 @@
 package biblioteca.biblioteca.controladores;
 
 
+import biblioteca.biblioteca.entidades.libros;
 import biblioteca.biblioteca.entidades.roles;
 import biblioteca.biblioteca.entidades.usuarios;
 import biblioteca.biblioteca.repositorios.RolesRepository;
 import biblioteca.biblioteca.repositorios.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,9 +25,7 @@ public class controladorUsuario {
 
     @Autowired
     public controladorUsuario(UsuarioRepository usuarioRepository, RolesRepository rolesRepository) {
-
         this.usuarioRepository = usuarioRepository;
-
         this.rolesRepository = rolesRepository;
     }
 
@@ -31,8 +33,18 @@ public class controladorUsuario {
      * Mostrar datos de los usuarios
      * *********************************************************************************/
     @GetMapping("/usuarios")
-    public String obtenerTodosLosUsuarios(Model model) {
-        model.addAttribute("listaUsuarios", usuarioRepository.findAll());
+    public String obtenerTodosLosUsuarios(
+            Model model,
+            @RequestParam(name = "pagina", required = false, defaultValue = "0") int pagina,
+            @RequestParam(name = "tamanio", required = false, defaultValue = "10") int tamanio
+    ) {
+        Pageable pageable = PageRequest.of(pagina, tamanio);
+        Page<usuarios> paginaUsuarios = usuarioRepository.findAll(pageable);
+        model.addAttribute("listaUsuarios", paginaUsuarios.getContent());
+        model.addAttribute("paginaActual", pagina);
+        model.addAttribute("totalPaginas", paginaUsuarios.getTotalPages());
+
+//        model.addAttribute("listaUsuarios", usuarioRepository.findAll());
         model.addAttribute("nuevoUsuario", new usuarios());
         model.addAttribute("usuarioModificado", new usuarios());
         model.addAttribute("listaRoles", rolesRepository.findAll()); // Agregar lista de roles al modelo
