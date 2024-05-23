68ce4b91a6af5cff12300d67994a37ff5c4c7ccd
VALENTIN PEREZ CUEVAS <vperez@ccoo.es>
2024-05-23 16:33:35 +0200

    Cambiada relación de listas y añadido nombre para que sea vea el nombre del creador

--- src/main/java/biblioteca/biblioteca/entidades/listas.java
+++ src/main/java/biblioteca/biblioteca/entidades/listas.java
@@ -12,12 +12,17 @@ public class listas {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id_lista;
-    private Integer id_usuario;
+
     private String nombre_lista;
     private Timestamp creado_desde;
     private Timestamp actualizado_desde;
     private Integer creado_por;
     private Integer actualizado_por;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario")
+    // Nombre de la columna en la tabla usuarios que hace referencia al idRol en la tabla roles
+    private usuarios usuario;
 
 
 
